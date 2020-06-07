6a5f98b21eaa27634902b56e619477b8814d81eb
Muhammad Arsalan <arsalan@MAKs-MacBook-Pro.local>
2020-06-07 08:36:20 +0500

    Added Send Mail Functionality

---
+++ blog/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+
+
+app_name = 'blog'
+
+urlpatterns = [
+
+    # Post Views
+
+    # path('', views.posts_list, name='posts_list'),
+    path('', views.PostListView.as_view(), name='posts_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_details, name='post_details'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+
+
+
+
+]
